1a58186bf5b80a3176b1382fddb7b435e1ed7f05
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-29 10:31:37 +0200

    Initial Commit

---
+++ StructClassActor/StructClassActorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructClassActorApp.swift
+//  StructClassActor
+//
+//  Created by Skorobogatow, Christian on 29/7/22.
+//
+
+import SwiftUI
+
+@main
+struct StructClassActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
